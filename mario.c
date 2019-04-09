bd237620c4d7504782a2259b8722399d9fd3ab24
ahalsey <xandros@linux.com>
2019-04-08 23:12:37 -0600

    Update mario.c
    added functionality for mario.c, builds a pyramid of # symbols based on user's suggested size

--- mario.c
+++ mario.c
@@ -1,1 +1,45 @@
 //mario.c
+#include <cs50.h>
+#include <stdio.h>
+void mario(int);
+    
+int main(void)
+{
+    int i = 0;
+    do
+    {
+        i = get_int("Enter pyramid height 1-8: ");
+        if (i < 1 || i > 8)
+        {
+            printf("Sorry, must be between 1 and 8\n");
+        }
+    }
+    while(i < 1 || i > 8);
+    mario(i);
+}
+
+void mario(int i)
+{
+    int j = 0;
+    int k;
+    int l;
+    while (j < i)
+    {
+        j++;
+        for (l = 8-j; l > 0; l--)
+        {
+            printf(" ");
+        }
+        for (k = 0; k < j; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (k = 0; k < j; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    //printf("%i\n",i);
+}
